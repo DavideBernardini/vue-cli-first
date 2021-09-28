cdcabaaa27153c583d2145d4fdff7a1b6382c508
DavideBernardini <davidebernardini09@gmail.com>
2021-09-28 18:13:22 +0200

    Aggiunto Title

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>La mia prima app con Vue CLI!</h2>
+</template>
+
+<script>
+export default {
+    name : 'Title'
+}
+</script>
+
+<style>
+    h2 {
+        font-size: 50px;
+    }
+</style>
